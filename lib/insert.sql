27e49d4df3abc46e8fca49ad88fb5a4149ee3330
webdevjscoder <cobianjuan19@gmail.com>
2020-05-11 15:24:58 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, gender, 
+color, temperament, alive) VALUES 
+("Mr. Chocolate", 10, "male", "chocolate brown", "playful", 1);
+INSERT INTO bears (name, age, gender, 
+color, temperament, alive) VALUES 
+("Rowdy", 6, "male", "light brown", "agressive", 1);
+INSERT INTO bears (name, age, gender, 
+color, temperament, alive) VALUES 
+("Tabitha", 5, "female", "white", "sensative", 1);
+INSERT INTO bears (name, age, gender, 
+color, temperament, alive) VALUES 
+("Sergeant Brown", 15, "male", "brown", "playful", 0);
+INSERT INTO bears (name, age, gender, 
+color, temperament, alive) VALUES 
+("Melissa", 3, "female", "light brown", "childish", 1);
+INSERT INTO bears (name, age, gender, 
+color, temperament, alive) VALUES 
+("Grinch", 10, "male", "brown", "agressive", 0);
+INSERT INTO bears (name, age, gender, 
+color, temperament, alive) VALUES 
+("Wendy", 4, "female", "black", "serious", 1);
+INSERT INTO bears (name, age, gender, 
+color, temperament, alive) VALUES 
+(NULL, NULL, NULL, "dark brown", "agressive", 1);
